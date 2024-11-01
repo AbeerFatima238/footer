d05033e0bc915111a20fe1105c583a9fe00e4acb
AbeerFatima238 <abeerfatimahom2@gmail.com>
2024-11-01 05:13:16 +0500

    first commit

---
+++ app/components/Footer.tsx
@@ -0,0 +1,20 @@
+export default function Footer() {
+    return (
+        <div>
+        <footer>
+        
+            <h2>Contact Us</h2>
+            <p>Phone: 923467578</p>
+            <h2>Email</h2>
+            <p>abc@gmail.com</p>
+            <h2>Visit Us At</h2>
+            <p>ABC Road, Spring Street, Karachi</p>
+            <hr />
+            <div className="flex items-center">
+            <h3>ALL RIGHTS RESERVED ®</h3>
+            </div>
+            </footer>
+            </div>
+        
+    );
+} 
